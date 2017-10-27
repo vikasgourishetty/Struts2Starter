958b82c31194ae87d43fa0b9974e57611c2f6537
vikasgourishetty <vikasgourishetty@gmail.com>
2017-10-27 15:03:06 -0400

    Adding Java resources.

---
+++ src/main/java/com/myapp/login/LoginAction.java
@@ -0,0 +1,9 @@
+package com.myapp.login;
+
+public class LoginAction {
+
+	public String execute(){
+		return "success";
+	}
+	
+}
---
+++ src/main/webapp/loginWelcome.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Logged in</h2>
+</body>
+</html>
